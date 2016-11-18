6deac84e3d1951dd96f08089c8ec88118390443a
Chris <chaslbeck3@gmail.com>
2016-11-18 14:27:45 -0500

    Add files via upload

---
+++ whilebox.c
@@ -0,0 +1,27 @@
+/*Chris Haslbeck*/
+#include <stdio.h>
+int main(void){
+
+	int height;
+	int width;
+
+	printf("Enter in a height: ");
+	scanf("%d",&height);
+	printf("Enter in a width: ");
+	scanf("%d",&width);
+
+	int yeet = 0;
+	int yaaa = 0;
+
+	while(yeet < height){
+		while(yaaa < width){
+			printf("\[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]");
+			yaaa++;
+		}
+	yaaa=0;
+	printf("\n");
+	yeet++;
+	}
+
+return 0;
+}
