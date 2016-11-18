11d16fb30b7ad85184549ec8907efa4ff385f992
Chris <chaslbeck3@gmail.com>
2016-11-18 14:36:11 -0500

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,15 @@
+/*Chris Haslbeck*/
+#include <stdio.h>
+int main(void) {
+
+	char foo =10;
+	char meme;
+	printf("Type something:");
+	scanf("%c", &meme);
+	while (meme!=foo) {
+	printf("Hi\n");
+
+}
+return 0;
+}
+
